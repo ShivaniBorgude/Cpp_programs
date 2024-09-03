4ae3ee2038c3dd240468f00267abb56308db00fa
ShivaniBorgude <shivani.borgude24@sanjivani.edu.in>
2024-09-03 21:50:34 +0530

    Add files via upload

---
+++ Program-8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a=6, b=4;
+
+//Equal operator
+cout<<"a==b is"<< (a==b)<<endl;
+
+//Greater than operater
+cout<<"a>b is"<< (a>b)<<endl;
+
+//Greater than or Equal to operater
+cout<<"a>=b is"<< (a>=b)<<endl;
+
+//Lesser than operater
+cout<<"a<b is"<< (a<b)<<endl;
+
+//Lesser than or equal to operater
+cout<<"a<=b is"<< (a<=b)<<endl;
+
+//true
+cout<<"a!=b is"<< (a!=b)<<endl;
+
+    return 0;
+}
