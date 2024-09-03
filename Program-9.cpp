9675c907a7e272cc3df3504a53058e02e8ad6c13
ShivaniBorgude <shivani.borgude24@sanjivani.edu.in>
2024-09-03 22:00:51 +0530

    Add files via upload

---
+++ Program-9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a=6, b=4;
+  //Logical AND operater
+  cout<<"a && b is"<<(a&&b)<<endl;
+  
+  //Logical OR operater
+  cout<<"a||b is"<<(a||b)<<endl;
+  
+  //Logical NOT operater
+  cout<<"a|b is"<<(!b)<<endl;
+
+    return 0;
+}
