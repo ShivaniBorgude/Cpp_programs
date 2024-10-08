f6130a27ced07da2f7ac9a70cb0c886520d882d7
ShivaniBorgude <shivani.borgude24@sanjivani.edu.in>
2024-10-08 23:47:41 +0530

    Add files via upload

---
+++ Round.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Rounding 4.6 gives:"<<round(4.6)<<endl;
+    cout <<"Rounding 8.4 gives:"<<round(8.4)<<endl;
+    return 0;
+}
